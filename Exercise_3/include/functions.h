014d6718cca00952a58c9fd496bbfe468f522719
ApChris <achris.info@gmail.com>
2020-11-30 00:07:26 +0200

    initial commit

---
+++ Exercise_3/include/functions.h
@@ -0,0 +1,12 @@
+#ifndef __FUNCTIONS_H_
+#define __FUNCTIONS_H_
+#include <cstddef>
+#include <iostream>
+#include <cstddef>
+using namespace std;
+bool SymmetricMatrix(long ** array, long n);
+bool DiagonalMatrix(long ** array, long n);
+bool UnitMatrix(long ** array, long n);
+void PrintArray(long ** array, long n);
+
+#endif
