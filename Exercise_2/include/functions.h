4f7c820405c15a28d756da6df683f3482057f968
ApChris <achris.info@gmail.com>
2020-11-29 22:20:14 +0200

    initial commit

---
+++ Exercise_2/include/functions.h
@@ -0,0 +1,10 @@
+#ifndef __FUNCTIONS_H_
+#define __FUNCTIONS_H_
+#include <cstddef>
+#include <iostream>
+#include <cstddef>
+using namespace std;
+bool CheckArray(long * array, long n);
+void PrintArray(long * array, long n);
+
+#endif
